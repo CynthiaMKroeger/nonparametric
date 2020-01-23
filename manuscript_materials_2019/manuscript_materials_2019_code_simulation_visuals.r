2c54d0494d2a073efd717d2fd99bcd050b9101e5
Cynthia M. Kroeger <cynthiamkroeger@gmail.com>
2020-01-23 17:06:12 +1100

    Change of Label

--- manuscript_materials_2019/manuscript_materials_2019_code_simulation_visuals.r
+++ manuscript_materials_2019/manuscript_materials_2019_code_simulation_visuals.r
@@ -22,7 +22,7 @@ arrows(c(1,3,5,7,9), lc[c(1:5),1], c(1,3,5,7,9), uc[c(1:5),1], angle = 90, lengt
 arrows(c(1,3,5,7,9), lc[c(1:5),2], c(1,3,5,7,9), uc[c(1:5),2], angle = 90, length = 0.02);arrows(c(1,3,5,7,9), uc[c(1:5),2], c(1,3,5,7,9), lc[c(1:5),2], angle = 90, length = 0.02)
 arrows(c(1,3,5,7,9), lc[c(1:5),3], c(1,3,5,7,9), uc[c(1:5),3], angle = 90, length = 0.02);arrows(c(1,3,5,7,9), uc[c(1:5),3], c(1,3,5,7,9), lc[c(1:5),3], angle = 90, length = 0.02)
 abline(h=0.1, lty=2);
-mtext('Degree of heteroscedasticity', side=1, line=1, cex=0.7)
+mtext('maximum standard error', side=1, line=1, cex=0.7)
 
 
 data = read.csv("type-I-error-0.05-g=5.csv",header=F)[,-1];Y=c(0.0,0.2)
@@ -42,7 +42,7 @@ arrows(c(1,3,5,7,9), lc[c(1:5),1], c(1,3,5,7,9), uc[c(1:5),1], angle = 90, lengt
 arrows(c(1,3,5,7,9), lc[c(1:5),2], c(1,3,5,7,9), uc[c(1:5),2], angle = 90, length = 0.02);arrows(c(1,3,5,7,9), uc[c(1:5),2], c(1,3,5,7,9), lc[c(1:5),2], angle = 90, length = 0.02)
 arrows(c(1,3,5,7,9), lc[c(1:5),3], c(1,3,5,7,9), uc[c(1:5),3], angle = 90, length = 0.02);arrows(c(1,3,5,7,9), uc[c(1:5),3], c(1,3,5,7,9), lc[c(1:5),3], angle = 90, length = 0.02)
 abline(h=0.05, lty=2);
-mtext('Degree of heteroscedasticity', side=1, line=1, cex=0.7)
+mtext('maximum standard error', side=1, line=1, cex=0.7)
 
 data = read.csv("type-I-error-0.01-g=5.csv",header=F)[,-1];Y=c(0.0,0.2)
 mu <- data[,c(1,4,7)]
@@ -62,7 +62,7 @@ arrows(c(1,3,5,7,9), lc[c(1:5),2], c(1,3,5,7,9), uc[c(1:5),2], angle = 90, lengt
 arrows(c(1,3,5,7,9), lc[c(1:5),3], c(1,3,5,7,9), uc[c(1:5),3], angle = 90, length = 0.02);arrows(c(1,3,5,7,9), uc[c(1:5),3], c(1,3,5,7,9), lc[c(1:5),3], angle = 90, length = 0.02)
 abline(h=0.01, lty=2);
 legend("topright", c("Kruskal-Wallis","Fisher's ANOVA","Welch's ANOVA"), lty = 1, pch=c(15,16,17), ncol=1, box.lty=0)
-mtext('Degree of heteroscedasticity', side=1, line=1, cex=0.7)
+mtext('maximum standard error', side=1, line=1, cex=0.7)
 dev.off()
 
 
